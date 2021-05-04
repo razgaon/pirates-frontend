32059ee406d7ba62f7433c1bcd81a8cb1affb627
razgaon <razgaon@gmail.com>
2021-05-03 22:58:22 -0400

    Added assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GameService} from './game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pirates-frontend';
+  gameCode: string;
+
+  constructor(private gameService: GameService) {
+
+  }
+
+  getGameRoom(): void {
+    if (this.gameCode) {
+      console.log(this.gameCode);
+    } else {
+      this.gameService.getGameCode()
+        .subscribe((data: string) => this.gameCode = data);
+    }
+
+  }
 }
